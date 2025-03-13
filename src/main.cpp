037432af9333cfb6f96d00a2aa48c2c67b34d9a5
Julian Steiner <juli.steiner@bluewin.ch>
2025-03-13 14:59:38 +0100

    Testing communication to board

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <Arduino.h>
 #include <Serial.h>
 
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("initialized serial");
+}
 
 void loop() {
   // put your main code here, to run repeatedly:
