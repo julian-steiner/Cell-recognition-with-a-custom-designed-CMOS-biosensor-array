c1a5c553239d795b97a2c349593e2b8dbefe69f9
Julian Steiner <juli.steiner@bluewin.ch>
2025-03-13 15:50:55 +0100

    tested register based gpio

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,21 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("initialized serial");
+
+  // Setup the output pins
+  pinMode(25, OUTPUT);
+  pinMode(27, OUTPUT);
+  pinMode(29, OUTPUT);
+  pinMode(31, OUTPUT);
+  pinMode(33, OUTPUT);
+  pinMode(35, OUTPUT);
+  pinMode(37, OUTPUT);
+  pinMode(38, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  // Set gpio pins with register
+  GPIOJ->ODR = (0b00000011);
 }
